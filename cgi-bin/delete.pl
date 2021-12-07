05c7d4cb5b8e90834dd5f2226e79a8c78c931ddb
ifbva2002 <ivelita@unsa.edu.pe>
2021-12-07 20:57:50 +0000

    Estructura de delete.pl terminada

--- cgi-bin/delete.pl
+++ cgi-bin/delete.pl
@@ -1,4 +1,39 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use DBI;
+use CGI;
+
+print "Content-type: text/html\n\n";
+print <<HTML;
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Página Eliminada</title>
+  </head>
+  <body>
+HTML
+
+#CGI part
+my $cgi = CGI->new;
+my $name = $cgi->param('name')
+
+#Database part
+my $user= 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.106";
+my $dbh = DBI->connect($dsn, $user, $password) or die ("No se puedo conectar");
+#Insertar datos
+my $sth = $dbh->prepare("DELETE FROM wiki WHERE name=?");
+$sth->execute($name);
+$dbh->disconnect;
+
+print <<HTML;
+    <h1 class="title-delete">Página eliminada con éxito</h1>
+    <h2 class="link-list"><a href="list.pl">Volver a la lista</a></h2>
+  </body>
+</html>
+HTML
+
 
