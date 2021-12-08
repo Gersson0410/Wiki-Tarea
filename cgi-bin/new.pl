91e9530a1f4e4181d82c7a882d37eca9f91e438b
Gersson Huichi <ghuichi@unsa.edu.pe>
2021-12-08 17:05:25 +0000

    Cambio semi total de la estructura new.pl para conectar con el view.pl

--- cgi-bin/new.pl
+++ cgi-bin/new.pl
@@ -4,38 +4,74 @@ use warnings;
 use CGI;
 use DBI;
 
-#CGI part
-#Cabecera
-print "Content-type: text/html\n\n";
-print <<HTML;
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Página Creada</title>
-  </head>
-  <body>
-HTML
+my $q = CGI->new;
+my $name = $q->param("name");
+my $markdown = $q->param("markdown");
+print $q->header('text/html;charset=UTF-8');
 
-my $cgi = CGI->new;
-my $name = $cgi->param('name');
-my $markdown = $cgi->param('markdown');
-
-print "<h1>$name</h1>\n";
-print "<p><pre>$markdown</pre></p>\n";
-
-#Database part
-my $user= 'alumno';
+my $user = 'alumno';
 my $password = 'pweb1';
-my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.106";
-my $dbh = DBI->connect($dsn, $user, $password) or die ("No se puedo conectar");
-#Insertar datos
-my $sth = $dbh->prepare("INSERT INTO wiki(name,markdown) VALUES(?,?)");
-$sth->execute($name, $markdown);
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.104";
+my $dbh = DBI->connect($dsn, $user, $password) or die("No se pudo conectar!");;
+
+my $sth = $dbh->prepare("SELECT name FROM Wiki WHERE name=?");
+$sth->execute($name);
+my @row;
+my @titles;
+while (@row = $sth->fetchrow_array){
+  push (@titles,@row);
+}
+$sth->finish;
+my $estado="";
+
+if($titles[0]eq($name)){
+  my $sth1 = $dbh->prepare ("UPDATE Wiki SET markdown=? WHERE name=?");
+  $sth1->execute($markdown, $name);
+  $sth1->finish;
+  $estado="Página actualizada";
+}
+else{
+  my $sth2 = $dbh->prepare("INSERT INTO Wiki (name, markdown) VALUES (?,?)");
+  $sth2->execute($name, $markdown);
+  $sth2->finish;
+  $estado="Página grabada";
+}
 $dbh->disconnect;
-print <<HTML;
-    <h2><span>Página grabada</span></h2>
-    <h2><a href="./list.pl">Listado de Páginas</h2>
-  </body>
-</html>
+
+my $body = renderBody($name,$markdown,$estado);
+print renderHTMLpage('Edit',$body);
+
+sub renderBody{
+  my $name = $_[0];
+  my $markdown = $_[1];
+  my $estado = $_[2];
+  my $body = <<"BODY";
+  <h1>$name</h1>
+    <pre>
+$markdown
+    </pre>
+    <hr>
+    <h2>$estado</h2>
+    <h3><a href="view.pl?name=$name">Ver Página</a> - <a href="list.pl">Listado de Páginas</a></h3>
+BODY
+  return $body;
+}
+
+sub renderHTMLpage{
+  my $title = $_[0];
+  my $body = $_[1];
+  my $html = <<"HTML";
+  <!DOCTYPE html>
+  <html lang="es">
+    <head>
+      <title>$title</title>
+      <link rel="stylesheet" href="../css/styles.css">
+      <meta charset="UTF-8">
+    </head>
+    <body>
+      $body
+    </body>
+  </html>
 HTML
+  return $html;
+}
