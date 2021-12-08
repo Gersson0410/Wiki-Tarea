28b811832bd66281d7d6aedb242d28760801d0f8
Gersson Huichi <ghuichi@unsa.edu.pe>
2021-12-08 01:30:23 +0000

    Estrucutra base del view.pl

--- cgi-bin/view.pl
+++ cgi-bin/view.pl
@@ -1,32 +1,69 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
- 
-my $line = <STDIN>;
-my $dict = ();
-print "\n";
+use CGI;
+use DBI;
 
-if($line=~/^#{6}(.+)/){
-   $dict=$1;
-     print "<h6>".$dict."</h6>";
-}elsif($line=~/^#{2}(.+)/){
-   $dict=$1;
-    print "<h2>".$dict."</h2>";
-}elsif($line=~/^#(.+)/){
-   $dict=$1;
-    print "<h1>".$dict."</h1>";
+my $param1 = CGI -> new;
+my $param2 = $param1 ->param('param1');
+
+my $user = 'alumno';
+my $password = 'pweb1';
+my $dsn = "DBI:MariaDB:database=pweb1;host=192.168.1.106";
+my $dbh = DBI->connect($dsn,$user,$password);
+my $sth = $dbh->prepare("SELECT markdown FROM Wiki WHERE name=?");
+$sth->execute($param2);
+my $texto = $sth->fetchrow_array;
+$sth->finish;
+
+$dbh->disconnect;
+
+print $param1->header('text/html;charset-UTF-8');
+print STDERR "$param2\n";
+
+my @splittedtext = splitString($texto);
+
+my $markdownHTML = generaMarkdown(@splittedtext);
+print STDERR "$markdownHTML\n";
+
+my $html = mostrarHTML($markdownHTML,$param2);
+print "$html\n";
+
+sub mostrarHTML{
+  my @textLines = @_;
+  my $markdownHTML = "";
+
+  foreach my $line(@textLines){
+ if($line=~/^######([^#]+)([a-zA-Z0-9]+)/){
+   $markdownHTML="<h6>".$1.$2."</h6>";
+}elsif($line=~/^##([^#]+)([a-zA-Z0-9]+)/){
+   $markdownHTML="<h2>".$1.$2."</h2>";
+}elsif($line=~/^#([^#]+)([a-zA-Z0-9]+)/){
+   $markdownHTML="<h1>".$1.$2."</h1>";
 }
 if($line=~/^[*]{3}(.+)[*]{3}$/){
-   $dict=$1;
-   print "<p><strong><em>".$dict."</em></strong></p>";
-}elsif($line=~/^[*]{2}(.+)[*]{2}$/){
-   $dict=$1;
-   print "<p><strong>".$dict."</strong></p>";
-}elsif($line=~/^[*]{1}(.+)[*]{1}$/){
-   $dict=$1;
-   print "<p><em>".$dict."</em></p>";
+   $markdownHTML="<p><strong><em>".$1."</em></strong></p>";
 }elsif($line=~/^[~]{2}(.+)[~]{2}$/){
-   $dict=$1;
-   print "<p>~".$dict."~~</p>";
+   $markdownHTML="<p>~".$1."~~</p>";
+}elsif($line=~/^[*]{2}(.+)[_](.+)[_](.+)[*]{2}$/){
+   $markdownHTML="<p><strong>".$1."<em>".$2."</em>".$3."</strong></p>";
+}elsif($line=~/^[*]{2}(.+)[*]{2}$/){
+   $markdownHTML="<p><strong>".$1."</strong></p>";
+}elsif($line=~/^```(.+)```$/){
+   $markdownHTML="<p><code>\n".$1."\n"."</code></p>";
+}elsif($line=~/^[*]{1}(.+)[*]{1}$/){
+   $markdownHTML="<p><em>".$1."</em></p>";
+}elsif($line=~/(.+)\[([a-zA-Z0-9]+)\]\(([a-zA-Z0-9]+)\)/){
+  $markdownHTML="<p>".$1."<a href='".$3."'>".$2."</a></p>";
+}else{
+  $markdownHTML="<p>".$line."</p>";
+} 
+ }
+return $markdownHTML; 
 }
-print "\n";
+
+sub splitString{
+  my $texto =$_[0];
+  my @splittedString = split(/\n/, $texto);
+  return @splittedString;
+} 
